7a8dceb81ba9847a6a027237b6b540e547f24348
Lucas Bortolazzo <lucas.dolberth@yahoo.com>
2020-09-23 21:15:02 -0300

    Changed debug config
    Changed debug config

--- fusion/settings.py
+++ fusion/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'd^^1qsayovduy)(r6b24o16vc(4f0hv)^xi7@+7x^_y(xyh$@l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
